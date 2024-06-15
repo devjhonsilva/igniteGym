07b8790eb08e0ef9a432f302fab41bc91467bd12
Jonatan Santos <jonatansilvasantos@outlook.com>
2024-06-15 20:56:45 -0300

    Criação tela SignIn e SignUp, Estilização de componentes Button, Input, Loading.

--- App.tsx
+++ App.tsx
@@ -1,18 +1,24 @@
-import { Text, View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from '@components/Loading';
+
+import { THEME } from 'src/theme';
+import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
 
       <StatusBar
         barStyle='light-content'
-        backgroundColor='transparent'
+        backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Text>Hello word</Text> : <View />}
+
+      {fontsLoaded ? <SignUp /> : <Loading />}
 
     </NativeBaseProvider>
   );
