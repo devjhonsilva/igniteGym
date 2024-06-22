0932d9a91fd3f3836d2436472d5d15335955fe67
Jonatan Santos <jonatansilvasantos@outlook.com>
2024-06-22 18:23:24 -0300

    Finalização do FrontEnd

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,102 +1,109 @@
-import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import { Controller, useForm } from 'react-hook-form';
 import { useNavigation } from "@react-navigation/native";
-import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
-import { useForm, Controller } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from "native-base";
+
+import { AuthNavigatorRoutesProps } from '@routes/auth.routes';
+//import { useAuth } from '@hooks/useAuth';
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 
+//import { AppError } from '@utils/AppError';
+
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { useState } from 'react';
 
-type FormDataProps = {
-    email: string;
-    password: string;
+type FormData = {
+  email: string;
+  password: string;
 }
 
-const signInSchema: any = yup.object({
-    email: yup.string().required('Informe o e-mail').email('E-mail inválido'),
-    password: yup.string().required('Informe a senha').min(6, 'A senha deve ter pelo menos 6 digítos')
-});
-
 export function SignIn() {
-    const navigation = useNavigation<AuthNavigatorRoutesProps>();
+  const [isLoading, setIsLoading] = useState(false)
 
-    const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
-        resolver: yupResolver(signInSchema)
-    });
+  //const { singIn } = useAuth();
+  const navigation = useNavigation<AuthNavigatorRoutesProps>();
+  const toas = useToast();
 
-    const handleNewAccount = () => {
-        navigation.navigate("signUp");
-    }
+  const { control, handleSubmit, formState: { errors } } = useForm<FormData>()
 
-    return (
-        <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
-            <VStack flex={1}>
-                <Image
-                    source={BackgroundImg}
-                    defaultSource={BackgroundImg}
-                    alt="Pessoas treinando"
-                    resizeMode="contain"
-                    position="absolute"
-                    top={-30}
-                />
+  function handleNewAccount() {
+    navigation.navigate('signUp');
+  }
 
-                <Center my={24}>
-                    <LogoSvg />
+  return (
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+        <VStack flex={1} px={10} pb={16}>
+        <Image
+          source={BackgroundImg}
+          defaultSource={BackgroundImg}
+          alt="Pessoas treinando"
+          resizeMode="contain"
+          position="absolute"
+        />
 
-                    <Text color="gray.100" fontSize="sm">
-                        Treine sua mente e o seu corpo.
-                    </Text>
-                </Center>
+        <Center my={24}>
+          <LogoSvg />
 
-                <Center pb={16} px={10}>
-                    <Center>
-                        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
-                            Acesse sua conta
-                        </Heading>
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo.
+          </Text>
+        </Center>
 
-                        <Controller
-                            control={control}
-                            name="email"
-                            render={({ field: { onChange, value } }) => (
-                                <Input
-                                    placeholder="E-mail"
-                                    keyboardType="email-address"
-                                    autoCapitalize="none"
-                                    onChangeText={onChange}
-                                    value={value}
-                                    errorMessage={errors.email?.message}
-                                />
-                            )}
-                        />
+        <Center>
+          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+            Acesse a conta
+          </Heading>
 
-                        <Controller
-                            control={control}
-                            name="password"
-                            render={({ field: { onChange, value } }) => (
-                                <Input
-                                    placeholder="Senha"
-                                    secureTextEntry
-                                    onChangeText={onChange}
-                                    value={value}
-                                    errorMessage={errors.password?.message}
-                                />
-                            )}
-                        />
-                    </Center>
-                    <Button title="Acessar" />
-                    <Text mt={200} color="white">Ainda não tem acesso?</Text>
-                    <Button
-                        title="Criar a sua conta"
-                        variant="outline"
-                        mt={2}
-                        onPress={handleNewAccount}
-                    />
-                </Center>
-            </VStack>
-        </ScrollView>
-    );
+          <Controller
+            control={control}
+            name="email"
+            rules={{ required: 'Informe o e-mail' }}
+            render={({ field: { onChange } }) => (
+              <Input
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                onChangeText={onChange}
+                errorMessage={errors.email?.message}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="password"
+            rules={{ required: 'Informe a senha' }}
+            render={({ field: { onChange } }) => (
+              <Input
+                placeholder="Senha"
+                secureTextEntry
+                onChangeText={onChange}
+                errorMessage={errors.password?.message}
+              />
+            )}
+          />
+
+          <Button
+            title="Acessar"
+            //onPress={handleSubmit(handleSignIn)}
+            isLoading={isLoading}
+          />
+        </Center>
+
+        <Center mt={24}>
+          <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
+            Ainda não tem acesso?
+          </Text>
+
+          <Button
+            title="Criar Conta"
+            variant="outline"
+            onPress={handleNewAccount}
+          />
+        </Center>
+      </VStack>
+    </ScrollView>
+  );
 }
