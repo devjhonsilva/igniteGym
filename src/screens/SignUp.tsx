c4407926221dc77b7fb0dc01214a355017839d6b
Jonatan Santos <jonatansilvasantos@outlook.com>
2024-06-28 16:57:22 -0300

    Introduzido API com Axios para comunicação com banco de dados

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,14 +1,17 @@
 import { useNavigation } from "@react-navigation/native";
-import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from "native-base";
 import { useForm, Controller } from 'react-hook-form';
-import {yupResolver} from '@hookform/resolvers/yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+
+import  {api}  from "@services/api";
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { AppError } from "@utils/AppError";
 
 type FormDataProps = {
   name: string;
@@ -18,13 +21,14 @@ type FormDataProps = {
 }
 
 const signUpSchema: any = yup.object({
-    name: yup.string().required('Informe o nome'),
-    email: yup.string().required('Informe o e-mail').email('E-mail inválido'),
-    password: yup.string().required('Informe a senha').min(6, 'A senha deve ter pelo menos 6 digítos'),
-    password_confirm: yup.string().required('Confirme a senha').oneOf([yup.ref('password')], 'A senha está diferente')
+  name: yup.string().required('Informe o nome'),
+  email: yup.string().required('Informe o e-mail').email('E-mail inválido'),
+  password: yup.string().required('Informe a senha').min(6, 'A senha deve ter pelo menos 6 digítos'),
+  password_confirm: yup.string().required('Confirme a senha').oneOf([yup.ref('password')], 'A senha está diferente')
 });
 
 export function SignUp() {
+  const toast = useToast();
 
   const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
     resolver: yupResolver(signUpSchema)
@@ -36,13 +40,25 @@ export function SignUp() {
     navigation.goBack();
   }
 
-  function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
-    console.log({ name, email, password, password_confirm })
+  async function handleSignUp({ name, email, password }: FormDataProps) {
+    try {
+      const response = await api.post('/users', { name, email, password })
+      console.log(response.data);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError ? error.message : 'Não foi possível criar a conta. Tente novamente mais tarde.';
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      });
+    }
   }
 
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
-        <VStack flex={1} px={10} pb={16}>
+      <VStack flex={1} px={10} pb={16}>
         <Image
           source={BackgroundImg}
           defaultSource={BackgroundImg}
